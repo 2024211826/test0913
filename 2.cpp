f2b39847ff8388b927a9a97b1b99c418e4519c51
2024211826 <2766358872@qq.com>
2025-09-13 22:38:48 +0800

    first save

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<double>a = {3.14, 2.71, 1.618};
+    for(double x : a){
+        cout << x << endl;
+    }
+    return 0;
+}
